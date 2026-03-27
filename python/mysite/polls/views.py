7370b2e00932b3e608b724236bbca955fb25c8be
Leonardo Brevi <leonardobrevi@gmail.com>
2026-03-27 01:26:05 +0100

    added detail template

--- python/mysite/polls/views.py
+++ python/mysite/polls/views.py
@@ -12,3 +12,8 @@ from django.http import HttpResponse
 def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     return HttpResponse(question.question_text)
+from django.shortcuts import render, get_object_or_404
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
